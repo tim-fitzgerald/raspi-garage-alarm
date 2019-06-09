fb105db6a99c1cd0379dadb852816fabf815577a
tim-fitzgerald <tim.fitzgerald@clio.com>
2019-06-08 18:19:01 -0700

    Black syntax format

--- garage_alarm.py
+++ garage_alarm.py
@@ -24,56 +24,45 @@ client = Client(twilio_sid, twilio_token)
 GPIO.setmode(GPIO.BCM)
 
 SENSORS = [
-        {"name": "Window 1",
-         "pin": 18,
-         "status": None,
-         "old_status": None
-        },
-        {
-         "name": "Window 2",
-         "pin": 24,
-         "status": None,
-         "old_status": None
-        },
-        {
-         "name": "Door",
-         "pin": 26,
-         "status": None,
-         "old_status": None
-        }
-        ]
+    {"name": "Window 1", "pin": 18, "status": None, "old_status": None},
+    {"name": "Window 2", "pin": 24, "status": None, "old_status": None},
+    {"name": "Door", "pin": 26, "status": None, "old_status": None},
+]
 
 for s in SENSORS:
-    GPIO.setup(s["pin"], GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    GPIO.setup(s["pin"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 ARMED = False
 
 isOpen = None
 oldIsOpen = None
 
-with open('numbers.json') as json_config:
+with open("numbers.json") as json_config:
     approved_numbers = json.load(json_config)
+
 
 def check_sensor(sensor):
     if GPIO.input(sensor):
-        status = 'open'
+        status = "open"
     else:
-        status = 'closed'
-        
+        status = "closed"
+
     return status
-    
+
+
 def send_alert(sensor):
     message = client.messages.create(
-            to=alarm_num,
-            from_= twilio_number,
-            body="Garage Alarm Triggered on sensor " + sensor["name"],
-            )
+        to=alarm_num,
+        from_=twilio_number,
+        body="Garage Alarm Triggered on sensor " + sensor["name"],
+    )
     print(message.sid)
+
 
 def alarm_loop():
     while True:
         global ARMED
-        ARMED=True
+        ARMED = True
         for sensor in SENSORS:
             sensor["old_status"] = sensor["status"]
             sensor["status"] = check_sensor(sensor["pin"])
@@ -83,71 +72,74 @@ def alarm_loop():
                     send_alert(sensor)
             else:
                 None
-                
+
         time.sleep(0.1)
+
 
 def validate_twilio_request(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         validator = RequestValidator(twilio_token)
         request_valid = validator.validate(
-                request.url,
-                request.form,
-                request.headers.get('X-TWILIO-SIGNATURE', ''))
+            request.url, request.form, request.headers.get("X-TWILIO-SIGNATURE", "")
+        )
         if request_valid:
             return f(*args, **kwargs)
         else:
             return abort(403)
+
     return decorated_function
 
-@app.route('/', methods = ['POST'])
+
+@app.route("/", methods=["POST"])
 @validate_twilio_request
 def index():
     global ARMED
     print(ARMED)
     return "OK"
 
-@app.route('/start', methods = ['POST'])
+
+@app.route("/start", methods=["POST"])
 @validate_twilio_request
 def start():
     global p
     global ARMED
     p = Process(target=alarm_loop)
     p.start()
-    ARMED=True
+    ARMED = True
     resp = MessagingResponse()
     resp.message("Alarm is now armed")
     return str(resp)
-    
 
-@app.route('/stop', methods = ['POST'])
+
+@app.route("/stop", methods=["POST"])
 @validate_twilio_request
 def stop():
     global p
     global ARMED
     p.terminate()
-    ARMED=False
+    ARMED = False
     resp = MessagingResponse()
     resp.message("Alarm is disarmed")
     return str(resp)
 
-@app.route('/sms', methods = ['GET','POST'])
+
+@app.route("/sms", methods=["GET", "POST"])
 @validate_twilio_request
 def sms():
     global ARMED
-    number = request.form['From'].lower()
-    message_body = request.form['Body'].lower()
+    number = request.form["From"].lower()
+    message_body = request.form["Body"].lower()
 
     print(number)
     if number not in approved_numbers.values():
         print("Number not permitted")
 
-
-    if message_body == 'arm':
-        return redirect(url_for('start'))
-    elif message_body == 'disarm':
-        return redirect(url_for('stop'))
-    elif message_body == 'state':
+    if message_body == "arm":
+        return redirect(url_for("start"))
+    elif message_body == "disarm":
+        return redirect(url_for("stop"))
+    elif message_body == "state":
         resp = MessagingResponse()
         if ARMED:
             resp.message(str("Armed"))
@@ -156,8 +148,8 @@ def sms():
         return str(resp)
     else:
         print("Invalid Option")
-        return redirect(url_for('index'))
-    
+        return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
